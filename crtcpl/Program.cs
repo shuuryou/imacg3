2f7c070aa5a1b014fb209f54f44ec91e39457cf6
Rocky HIll <qbancoffee@yahoo.com>
2020-05-12 13:22:52 -0400

    added lines to kill any reminaing threads when running with mono

--- crtcpl/Program.cs
+++ crtcpl/Program.cs
@@ -102,6 +102,13 @@ namespace crtcpl
             {
                 m.ReleaseMutex();
                 m.Dispose();
+                /*
+                 * With mono, there are some threads that remain when the application is closed
+                 * preventing the application from closing and causing to seem like it's hanging.
+                 * The two following calls remedy this.
+                 **/
+                Application.Exit();
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
             }
         }
     }
