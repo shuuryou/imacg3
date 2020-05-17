3960bb83cc4c073eb68a58ebe674fa29bbe7c34d
Rocky HIll <qbancoffee@yahoo.com>
2020-05-17 16:43:35 -0400

    fix max value for Brightness

--- crtcpl/Constants.cs
+++ crtcpl/Constants.cs
@@ -80,7 +80,7 @@
         public const int IVAD_PARALLELOGRAM_MIN = 0x80; // Most left
         public const int IVAD_PARALLELOGRAM_MAX = 0xFF; // Most right
         public const int IVAD_BRIGHTNESS_MIN = 0x00; // Most dim
-        public const int IVAD_BRIGHTNESS_MAX = 0x32; // Most bright
+        public const int IVAD_BRIGHTNESS_MAX = 0x0A; // Most bright
         public const int IVAD_ROTATION_MIN = 0x00; // Most left
         public const int IVAD_ROTATION_MAX = 0x7F; // Most right
     }
